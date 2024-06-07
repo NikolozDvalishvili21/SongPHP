ea650674dc4d2b55e586502e396ea4bcb840e299
Nika <nikadvalishvili517@gmail.com>
2024-06-08 03:36:06 +0400

    made top5 display

--- src/components/Songs/index.jsx
+++ src/components/Songs/index.jsx
@@ -3,7 +3,30 @@ import "../Songs/style.css";
 import play from "../../icons/icons8-play-30.png";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
 const TopSongs = () => {
+  const [topSongs, setTopSongs] = useState([]);
+
+  useEffect(() => {
+    const fetchTopSongs = async () => {
+      try {
+        const response = await axios.get('http://localhost/Project/top5_songs.php');
+        if (response.data.status === 'success') {
+          setTopSongs(response.data.songs);
+        } else {
+          console.error('Error fetching top songs:', response.data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching top songs:', error);
+      }
+    };
+
+    fetchTopSongs();
+  }, []);
+
   return (
     <>
       <div className="with-button">
@@ -12,94 +35,25 @@ const TopSongs = () => {
             Top Songs
           </h1>
           <div className="content_songs">
-            <div className="first-song">
-              <div className="song-info">
-                <div className="song-title">
-                  <h2 style={{ fontSize: "20px", fontWeight: "700" }}>PRIDE</h2>
+            {topSongs.map((song, index) => (
+              <div className="first-song" key={index}>
+                <div className="song-info">
+                  <div className="song-title">
+                    <h2 style={{ fontSize: "20px", fontWeight: "700" }}>{song.SongName}</h2>
+                  </div>
+                  <div className="song-author">
+                    <h4>{song.FirstName} {song.LastName}</h4>
+                  </div>
                 </div>
-                <div className="song-author">
-                  <h4>Kendrick Lamar</h4>
-                </div>
-              </div>
-              <div className="play-button">
-                <img
-                  src={play}
-                  style={{ width: "30px", height: "30px", cursor: "pointer" }}
-                />
-              </div>
-            </div>
-            <div className="first-song">
-              <div className="song-info">
-                <div className="song-title">
-                  <h2 style={{ fontSize: "20px", fontWeight: "700" }}>
-                    2 Milli
-                  </h2>
-                </div>
-                <div className="song-author">
-                  <h4>Soulja Boy</h4>
+                <div className="play-button">
+                  <img
+                    src={play}
+                    style={{ width: "30px", height: "30px", cursor: "pointer" }}
+                    alt="Play Button"
+                  />
                 </div>
               </div>
-              <div className="play-button">
-                <img
-                  src={play}
-                  style={{ width: "30px", height: "30px", cursor: "pointer" }}
-                />
-              </div>
-            </div>
-            <div className="first-song">
-              <div className="song-info">
-                <div className="song-title">
-                  <h2 style={{ fontSize: "20px", fontWeight: "700" }}>
-                    Comfortably Numb
-                  </h2>
-                </div>
-                <div className="song-author">
-                  <h4>Pink Floyd</h4>
-                </div>
-              </div>
-              <div className="play-button">
-                <img
-                  src={play}
-                  style={{ width: "30px", height: "30px", cursor: "pointer" }}
-                />
-              </div>
-            </div>
-            <div className="first-song">
-              <div className="song-info">
-                <div className="song-title">
-                  <h2 style={{ fontSize: "20px", fontWeight: "700" }}>
-                    Plastic Trees
-                  </h2>
-                </div>
-                <div className="song-author">
-                  <h4>Radiohead</h4>
-                </div>
-              </div>
-              <div className="play-button">
-                <img
-                  src={play}
-                  style={{ width: "30px", height: "30px", cursor: "pointer" }}
-                />
-              </div>
-            </div>
-            <div className="first-song">
-              <div className="song-info">
-                <div className="song-title">
-                  <h2 style={{ fontSize: "20px", fontWeight: "700" }}>
-                    I'm God
-                  </h2>
-                </div>
-                <div className="song-author">
-                  <h4>Lil B</h4>
-                </div>
-              </div>
-              <div className="play-button">
-                <img
-                  src={play}
-                  style={{ width: "30px", height: "30px", cursor: "pointer" }}
-                />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
         <Nav.Link href="#TopSongs" as={Link} to="/topsongs">
